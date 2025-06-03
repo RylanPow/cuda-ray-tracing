6a26262301c1606355de8fb53bbff4f77d59a7a8
Rylan Pow <powrylan@gmail.com>
2025-06-02 20:22:06 -0700

    hit_sphere() fixes, correct circle now

--- main.cpp
+++ main.cpp
@@ -3,13 +3,12 @@
 #include "ray.h"
 #include <fstream>
 
-bool hit_sphere(const vec3& center, float radius, const ray& r) {
+float hit_sphere(const vec3& center, float radius, const ray& r) {
     vec3 oc = r.origin() - center;
     float a = dot(r.direction(), r.direction());
     float b = 2.0 * dot(oc, r.direction());
     float c = dot(oc, oc) - radius*radius;
     float discriminant = b*b - 4*a*c;
-    return (discriminant > 0);
     if (discriminant < 0) {
         return -1.0;
     }
