b7e0d5a8a0f070c5962e4375d0fef9640995145a
Rylan Pow <powrylan@gmail.com>
2025-06-02 19:16:19 -0700

    sphere shaded but colors appear wrong

--- main.cpp
+++ main.cpp
@@ -10,6 +10,12 @@ bool hit_sphere(const vec3& center, float radius, const ray& r) {
     float c = dot(oc, oc) - radius*radius;
     float discriminant = b*b - 4*a*c;
     return (discriminant > 0);
+    if (discriminant < 0) {
+        return -1.0;
+    }
+    else {
+        return (-b - sqrt(discriminant)) / (2.0*a);
+    }
 }
 // sphere at C with radius R is: (x - cx)^2 + (y - cy)^2 + (z - cz)^2 = R^2
 // in vector form, vector from center to radius of sphere is (p - C)
@@ -24,11 +30,20 @@ bool hit_sphere(const vec3& center, float radius, const ray& r) {
 
 
 vec3 color(const ray& r) {
-    if (hit_sphere(vec3(0, 0, -1), 0.5, r))
-        return vec3(1, 0, 0);
+    float t = hit_sphere(vec3(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0,0,-1));
+        return 0.5*vec3(N.x()+1, N.y()+1, N.z()+1);
+    }
     vec3 unit_direction = unit_vector(r.direction());
-    float t = 0.5 * (unit_direction.y() + 1.0);
+    t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+
+    // if (hit_sphere(vec3(0, 0, -1), 0.5, r))
+    //     return vec3(1, 0, 0);
+    // vec3 unit_direction = unit_vector(r.direction());
+    // float t = 0.5*(unit_direction.y() + 1.0);
+    // return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
 }
 
 
