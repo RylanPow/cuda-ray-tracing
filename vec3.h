fb799cd4910bf9a43b220613d5ed43318c751919
RylanPow <powrylan@gmail.com>
2025-07-14 03:32:48 -0700

    vec3 CUDA annotations

--- vec3.h
+++ vec3.h
@@ -8,6 +8,9 @@
 
 class vec3  {
     public:
+
+        //note: vec3 gets used by both host and device, so get annotated with both
+
         __host__ __device__ vec3() {}_host__ __device__ vec3(float e0, float e1, float e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
         __host__ __device__ inline float x() const { return e[0]; }
         __host__ __device__ inline float y() const { return e[1]; }
