e162f76ab887c30cb43591cafbedb45236dcf4a6
RylanPow <powrylan@gmail.com>
2025-07-14 05:32:16 -0700

    camera CUDA translation

--- camera.h
+++ camera.h
@@ -1,22 +1,28 @@
 #ifndef CAMERAH
 #define CAMERAH
 
+#include <curand_kernel.h>
 #include "ray.h"
 
-vec3 random_in_unit_disk() {
+#define M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+
+__device__ vec3 random_in_unit_disk(curandState *local_rand_state) {
     vec3 p;
     do {
-        p = 2.0*vec3(drand48(), drand48(), 0) - vec3(1, 1, 0);
-    } while (dot(p, p) >= 1.0);
+        p = 2.0f*vec3(curand_uniform(local_rand_state), curand_uniform(local_rand_state), 0) - vec3(1, 1, 0);
+    } while (dot(p, p) >= 1.0f);
     return p; 
 }
 
 class camera {
     public:
-        camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect, float aperture, float focus_dist) {
-            lens_radius = aperture / 2;
-            float theta = vfov*M_PI/180;
-            float half_height = tan(theta/2);
+        __device__ camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect, float aperture, float focus_dist) {
+            lens_radius = aperture / 2.0f;
+            float theta = vfov*((float)M_PI)/180.0f;
+            float half_height = tan(theta/2.0f);
             float half_width = aspect * half_height;
             origin = lookfrom;
             w = unit_vector(lookfrom - lookat);
@@ -24,11 +30,11 @@ class camera {
             v = cross(w, u);
 
             lower_left_corner = origin - half_width*focus_dist*u -half_height*focus_dist*v - focus_dist*w;
-            horizontal = 2 * half_width * focus_dist * u;
-            vertical = 2 * half_height * focus_dist * v;
+            horizontal = 2.0f * half_width * focus_dist * u;
+            vertical = 2.0f * half_height * focus_dist * v;
         }
-        ray get_ray(float s, float t) { 
-            vec3 rd = lens_radius*random_in_unit_disk();
+        __device__ ray get_ray(float s, float t, curandState *local_rand_state) { 
+            vec3 rd = lens_radius*random_in_unit_disk(local_rand_state);
             vec3 offset  = u * rd.x() + v * rd.y();
             return ray(origin + offset, lower_left_corner + s*horizontal + t*vertical - origin - offset); 
         }
