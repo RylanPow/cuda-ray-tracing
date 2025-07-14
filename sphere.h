793442bc4a44b68c7163fe709768f896025a4eca
RylanPow <powrylan@gmail.com>
2025-07-14 03:41:42 -0700

    CUDA annotated sphere.h

--- sphere.h
+++ sphere.h
@@ -3,24 +3,24 @@
 
 #include "hitable.h"
 
-class sphere : public hitable {
+class sphere: public hitable  {
     public:
-        sphere() {}
-        sphere(vec3 cen, float r, material *m) : center(cen), radius(r), mat_ptr(m) {};
-        virtual bool hit(const ray& r, float t_min, float tmax, hit_record& rec) const;
+        __device__ sphere() {}
+        __device__ sphere(vec3 cen, float r, material *m) : center(cen), radius(r), mat_ptr(m)  {};
+        __device__ virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
         vec3 center;
         float radius;
         material *mat_ptr;
 };
 
-bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
+__device__ bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
     vec3 oc = r.origin() - center;
     float a = dot(r.direction(), r.direction());
     float b = dot(oc, r.direction());
     float c = dot(oc, oc) - radius*radius;
-    float discriminant = b*b - a * c;
+    float discriminant = b*b - a*c;
     if (discriminant > 0) {
-        float temp = (-b - sqrt(b*b - a*c)) / a;
+        float temp = (-b - sqrt(discriminant))/a;
         if (temp < t_max && temp > t_min) {
             rec.t = temp;
             rec.p = r.point_at_parameter(rec.t);
@@ -28,7 +28,7 @@ bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const 
             rec.mat_ptr = mat_ptr;
             return true;
         }
-        temp = (-b + sqrt(b*b - a*c))/a;
+        temp = (-b + sqrt(discriminant)) / a;
         if (temp < t_max && temp > t_min) {
             rec.t = temp;
             rec.p = r.point_at_parameter(rec.t);
@@ -39,5 +39,6 @@ bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const 
     }
     return false;
 }
+
 
 #endif
